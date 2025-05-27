90d85ac33fe5230b68b7feae8d9aaacc9ff10865
LVgit12 <tdsgithub@gmail.com>
2025-05-27 15:21:55 -0300

    Add files via upload

---
+++ industria1/desempenho.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale-1">
+        <meta http-equiv="content-language" content="pt-br">
+        <title>PHP / Array</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <style>
+        .user{
+            float: right;
+        }
+        body{
+            background:  #2a2b38;
+        }
+
+        @keyframes gradient {
+            0% {
+            background-position: 0% 50%;
+            }
+            50% {
+            background-position: 100% 50%;
+            }
+            100% {
+            background-position: 0% 50%;
+        }
+        }
+
+    </style>
+    <body>
+        <div class="card-body" style="background-color: #ffeba7">    
+            <center><h2><b>DESEMPENHO</b></h2></center>
+        </div> 
+    </body>
