92cbf10187702407048c2a17ed098c2f099de718
LVgit12 <tdsgithub@gmail.com>
2025-05-27 17:05:54 -0300

    Add files via upload

---
+++ industria1/grafico.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Ano', 'Fabricados', 'Retrabalho', 'Perda'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Gráfico de Acompanhamento',
+            subtitle: 'Fabricados, Retrabalho, Perda: 2014-2017',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    </script>
+  </head>
+  <body>
+    <div id="columnchart_material" style="width: 600px; height: 250px;"></div>
+  </body>
+</html>
--- industria1/inicial.php
+++ industria1/inicial.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+    $data = date("d/m/Y");
+    $_SESSION['data'] = $data;
     if(!isset($_SESSION['usuario'])){
         header('Location:index.php');
     }
@@ -23,7 +25,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale-1">
         <meta http-equiv="content-language" content="pt-br">
-        <title>PHP / Array</title>
+        <title>Sapataria</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
@@ -59,23 +61,37 @@
             </div>
         </nav>
         <br><br>
-        <center><h2><font color="white">SEJA BEM-VINDO, <?php echo $nomes[$id] ?>!</font></h2></center>
+        <center><h2><font color="white">Olá, <?php echo $nomes[$id] ?>!</font></h2></center>
         <br/><br/>
-        <div class="row justify-content-center row-cols-1 row-cols-md-3 text-center">
+        <div class="row justify-content-center row-cols-2 row-cols-md-3 text-center">
+            <div class="cols">
                 <div class="card mb-4 rounded shadow-sw">
-                    <div class="card-header py-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000080" class="bi bi-people-fill" viewBox="0 0 16 16">
-                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
-                        </svg><br/>
-                        <h3><font color="black"><b>DADOS RECENTES</b></font></h3>
+                    <div class="card-header py-3" style="background-color: #ffeba7">
+                        <h3><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
+                        <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1z"/>
+                        </div>
+                        </svg>&nbsp;&nbsp;<b>GRÁFICO DO DIA: <?php echo date("d/m/Y") ?></b></h3>
+                    <div class="card-body">
+                       <?php
+                            include "grafico2.php";
+                       ?>
+                    </div>
+                </div>
+            </div>
+            <div class="cols">
+                <div class="card mb-4 rounded shadow-sw">
+                    <div class="card-header py-3" style="background-color: #ffeba7">
+                        <h3><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
+                        <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1z"/>
+                        </svg>&nbsp;&nbsp;<b></b></h3>
                     </div>
                     <div class="card-body">
                         <?php
-                            
+                            include "grafico.php";
                         ?>
                     </div>
+                </div>
             </div>
-        </div>
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -85,7 +101,7 @@
                     </div>
                     <div class="modal-body text-start">
                         <form action="cadastro.php" method="post">
-                            <label class="form-label">QUANTIDADE DE FUNCIONÁRIOS DO DIA</label>
+                            <label class="form-label">QUANTIDADE DE FUNCIONÁRIOS PRESENTES NO DIA</label>
                             <input class="form-control" type="number" name="nome" required>
                             </br>
                             <label class="form-label">NÚMERO DE UNIDADES PRODUZIDAS</label>
@@ -97,7 +113,7 @@
                             <label class="form-label">NÚMERO DE DEFEITOS</label>
                             <input class="form-control" type="number" name="senha" required >
                             </br>
-                            <input type="submit" class="btn btn-success" value="CADASTRAR">
+                            <input type="submit" class="btn btn-success" value="GERAR GRÁFICO">
                         </form>
                     </div>
                     <div class="modal-footer">
